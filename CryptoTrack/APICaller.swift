c6cf9750e7e1dd59de745afc9c43bdf0d0768c9b
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-03-27 13:55:48 +0000

    Fixed compilation errors

--- CryptoTrack/APICaller.swift
+++ CryptoTrack/APICaller.swift
@@ -12,4 +12,9 @@ final class APICaller {
     
     private struct Constants {
         static let apiKey = "603D6C0D-5B5F-4E19-9657-A4DE6374AACB"
-        static let assetsEndpoint = "https://rest.coinapi.io/v1/asset
+        static let assetsEndpoint = "https://rest.coinapi.io/v1/assets/"
+    }
+    
+    private init() {}
+    
+    public var ico
