fbc802f8ad4b79436979fba5a8f353163ad6ec41
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-05-17 14:11:43 +0000

    USB: cypress_m8: fix ring-indicator detection and reporting

--- CryptoTrack/APICaller.swift
+++ CryptoTrack/APICaller.swift
@@ -66,4 +66,6 @@ final class APICaller {
     
     public func getAllIcons() {
         guard let url = URL(string:
-                "https://rest.coinapi.io/v1/assets/icons/55/?apikey=603D6
+                "https://rest.coinapi.io/v1/assets/icons/55/?apikey=603D6C0D-5B5F-4E19-9657-A4DE6374AACB")
+                else {
+     
