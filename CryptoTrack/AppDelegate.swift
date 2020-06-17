9aacfe54f1108b8ebc97abfa03566873430192e3
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-06-17 14:51:20 +0000

    First step

--- CryptoTrack/AppDelegate.swift
+++ CryptoTrack/AppDelegate.swift
@@ -12,4 +12,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptions
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for cust
