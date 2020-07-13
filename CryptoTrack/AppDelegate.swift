da3c549a01178e2f1355ecf1d4144ebebfd73a59
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-07-13 20:51:06 +0000

    Fix broken test case

--- CryptoTrack/AppDelegate.swift
+++ CryptoTrack/AppDelegate.swift
@@ -33,4 +33,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If any sessions were disc
+        // If any sessions were discarded while the application was not running, this will be c
