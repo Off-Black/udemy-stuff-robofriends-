395e84fddebe0c0543f066fa38f8b751aa28516b
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-08-30 21:35:14 +0000

    docs: add python 2 sunset banner to documentation (#9036)

--- CryptoTrack/CryptoTableViewCell.swift
+++ CryptoTrack/CryptoTableViewCell.swift
@@ -25,3 +25,66 @@ class CryptoTableViewCellModel {
         self.symbol = symbol
         self.price = price
         self.iconUrl = iconUrl
+    }
+}
+
+class CryptoTableViewCell: UITableViewCell {
+    static let identifier = "CryptoTableViewCell"
+    
+    // Subviews
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24, weight: .medium)
+        return label
+    }()
+    
+    private let symbolLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemOrange
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+     
+    private let priceLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGreen
+        label.textAlignment = .right
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(symbolLabel)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(iconImageView)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // Layout
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let size: CGFloat = contentView.frame.size.height/1.1
+        iconImageView.frame = CGRect(
+            x: 20,
+            y: (contentView.frame.size.height-size)/2,
+            width: size,
+            height: size
+        )
+        
