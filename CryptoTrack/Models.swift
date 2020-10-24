9d16324513e07628b0a121d27e0936a62f1097e8
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-10-24 15:44:14 +0000

    x86/apic: Serialize LVTT and TSC_DEADLINE writes

--- CryptoTrack/Models.swift
+++ CryptoTrack/Models.swift
@@ -27,4 +27,5 @@ struct Icon: Codable {
 //    "data_quote_start": "2014-02-24T17:43:05.0000000Z",
 //    "data_quote_end": "2021-06-15T23:31:41.3040078Z",
 //    "data_orderbook_start": "2014-02-24T17:43:05.0000000Z",
-//    "da
+//    "data_orderbook_end": "2020-08-05T14:38:38.3413202Z",
+//    "data_trade_star
