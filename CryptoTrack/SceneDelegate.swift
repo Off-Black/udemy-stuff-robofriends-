1db9413e2bca95953ef3dc9314b4f0ef1f5bf19a
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-11-01 21:53:59 +0000

    update data files

--- CryptoTrack/SceneDelegate.swift
+++ CryptoTrack/SceneDelegate.swift
@@ -13,4 +13,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally c
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindo
