645d2aa413467237970ba9d2dae179c2c16db37b
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-11-28 09:20:21 +0000

    Update lab5.md

--- CryptoTrack/SceneDelegate.swift
+++ CryptoTrack/SceneDelegate.swift
@@ -22,4 +22,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the sc
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was no
