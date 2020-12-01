dda102090635cb641995bf8ef377b2e5d4649f33
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-12-01 20:05:25 +0000

    config updates

--- CryptoTrack/SceneDelegate.swift
+++ CryptoTrack/SceneDelegate.swift
@@ -27,4 +27,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from a
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this met
