ceaa4350a588eb72d76214525a5d7b5c884b96c6
Off-Black <107740289+Off-Black@users.noreply.github.com>
2020-12-13 20:22:19 +0000

    Add new components

--- CryptoTrack/SceneDelegate.swift
+++ CryptoTrack/SceneDelegate.swift
@@ -36,4 +36,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
 
-    func scen
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called
