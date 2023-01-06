28da2700ee86ee78360433ee858d05cbb586e3a0
Off-Black <107740289+Off-Black@users.noreply.github.com>
2023-01-06 21:51:13 +0000

    fix formatting error

--- CryptoTrackUITests/CryptoTrackUITests.swift
+++ CryptoTrackUITests/CryptoTrackUITests.swift
@@ -12,4 +12,7 @@ class CryptoTrackUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best to stop immediately when a failur
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’
