1a8ddb2a73400cec3feb2b162c6512f6df2b327d
Off-Black <107740289+Off-Black@users.noreply.github.com>
2023-01-06 14:20:10 +0000

    Improvments

--- CryptoTrackUITests/CryptoTrackUITests.swift
+++ CryptoTrackUITests/CryptoTrackUITests.swift
@@ -10,4 +10,6 @@ import XCTest
 class CryptoTrackUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the inv
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI
