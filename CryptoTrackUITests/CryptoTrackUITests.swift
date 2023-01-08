c53f5dc83d5fcfad4c70b5185ba70e095d9b0f4d
Off-Black <107740289+Off-Black@users.noreply.github.com>
2023-01-08 09:26:31 +0000

    New translations default.json (Portuguese)

--- CryptoTrackUITests/CryptoTrackUITests.swift
+++ CryptoTrackUITests/CryptoTrackUITests.swift
@@ -15,4 +15,4 @@ class CryptoTrackUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - 
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do 
