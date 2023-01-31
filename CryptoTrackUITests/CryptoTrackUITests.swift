555c79d73df44177f2d959393ae3b8e10ca9da12
Off-Black <107740289+Off-Black@users.noreply.github.com>
2023-01-31 06:13:43 +0000

    minor typo correction

--- CryptoTrackUITests/CryptoTrackUITests.swift
+++ CryptoTrackUITests/CryptoTrackUITests.swift
@@ -19,4 +19,7 @@ class CryptoTrackUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of 
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws
