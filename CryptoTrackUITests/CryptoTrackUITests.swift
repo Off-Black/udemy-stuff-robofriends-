7262d925ce3d971c72bebd1002a23745016309de
Off-Black <107740289+Off-Black@users.noreply.github.com>
2023-02-04 06:06:47 +0000

    Input: i8042 - add noloop quirk for Asus X750LN

--- CryptoTrackUITests/CryptoTrackUITests.swift
+++ CryptoTrackUITests/CryptoTrackUITests.swift
@@ -22,4 +22,5 @@ class CryptoTrackUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws
+    func testExample() throws {
+        // UI tests must launch the application that they tes
