577c10170392d58c7410a010d11778573ba90d22
Off-Black <107740289+Off-Black@users.noreply.github.com>
2023-02-23 09:28:44 +0000

    fdi-setup

--- CryptoTrackUITests/CryptoTrackUITests.swift
+++ CryptoTrackUITests/CryptoTrackUITests.swift
@@ -33,4 +33,4 @@ class CryptoTrackUITests: XCTestCase {
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-         
+            // This measures how long it takes to launch your applicat
