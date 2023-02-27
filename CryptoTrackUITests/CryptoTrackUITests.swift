9fbf2ffe6bb751113630653b1a49374740b03b9d
Off-Black <107740289+Off-Black@users.noreply.github.com>
2023-02-27 21:44:34 +0000

    Updated pl translation

--- CryptoTrackUITests/CryptoTrackUITests.swift
+++ CryptoTrackUITests/CryptoTrackUITests.swift
@@ -36,4 +36,7 @@ class CryptoTrackUITests: XCTestCase {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
-    
+            }
+        }
+    }
+}
